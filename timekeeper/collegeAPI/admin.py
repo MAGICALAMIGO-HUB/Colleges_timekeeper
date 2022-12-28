4b6ccdd3489c5b78601a155aa54d28ae2b26062a
Anish Kumar shrestha <np01cp4a190413@islingtoncollege.edu.np>
2022-12-28 22:40:18 +0545

    creating user model

--- timekeeper/collegeAPI/admin.py
+++ timekeeper/collegeAPI/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import College
+from .models import College,User
 admin.site.register(College)
+admin.site.register(User)
 # Register your models here.
--- timekeeper/collegeAPI/models.py
+++ timekeeper/collegeAPI/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.utils import timezone
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFit
 class College(models.Model):
     college_ID = models.CharField(max_length=255, unique=True)
     college_name = models.CharField(max_length=255)
@@ -13,3 +15,31 @@ class College(models.Model):
     user_ID_initial = models.CharField(max_length=255)
     def __str__(self):
         return (self.college_name.capitalize()+", "+(self.location).split(',')[0].capitalize()+", "+self.city.capitalize()+", "+self.country.capitalize()+", "+str(self.year_founded)[0:4])
+
+
+
+class User(models.Model):
+    user_id =models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    hometown_address = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    address_long_lat = models.TextField()
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=1)
+    role = models.CharField(max_length=255)
+    emergency_call = models.CharField(max_length=255)
+    citizenship = models.ImageField(upload_to='citizenship_images/')
+    profile_image = models.ImageField(upload_to='profile_images/')
+    thumbnail = ImageSpecField(source='profile_image',
+                               processors=[ResizeToFit(100, 100)],
+                               format='JPEG',
+                               options={'quality': 60})
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (str(self.user_id)+". "+self.email)
